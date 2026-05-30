a3886f220d6b6bbcec8360eca8b136f5ca964593
soul2024quest <kaustubhgaur134@gmail.com>
2026-05-30 10:31:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import Bubbles from '@/components/Bubbles'
-import styles from './page.module.css'
+import styles from './pages.module.css'
 
 const pages = [
   {
